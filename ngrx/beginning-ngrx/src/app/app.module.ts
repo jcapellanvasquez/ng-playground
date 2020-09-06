0791e46c9a90e3b7ed97284d4cb0163e2075f529
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2020-09-06 19:59:54 -0400

    add product module and #primeng dependency

--- ngrx/beginning-ngrx/src/app/app.module.ts
+++ ngrx/beginning-ngrx/src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+      }
+    ], {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
